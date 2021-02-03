a59a7085e37561df6ee193539be092d3e6dbf026
qu2001 <qu2001@example.com>
2021-02-03 10:56:14 +0100

    add draw_line function

--- main.c
+++ main.c
@@ -14,8 +14,23 @@ typedef struct polygo {
 
 
 /* Put pixel at x,y with 8-bit color-depth. */
-void putpx(SDL_Surface *surf, int x, int y, Uint8 color) {
-    ( (Uint8*)surf->pixels+y*WINDOW_HEIGHT)[x] = color;
+void put_pixel(SDL_Surface *surf, int x, int y, Uint8 color) {
+    ( (Uint8*)surf->pixels+y*surf->w)[x] = color;
+}
+
+void draw_line(SDL_Surface *surf, float x1, float x2, float y1, float y2, Uint8 color) {
+    /*
+    Rasterizing line by casting coordinates to int. Using linear equation to calculate
+    points. Consider y-Axis pointing downwards. */
+    float m = (y2 - y1) / (x1 - x2); /* slope */
+    float b = m*x2 + y2; /* y-intersect */
+    
+    float x, y;
+    for(x=x1; x<x2; x+=1) {
+        y = -m*x + b; /* linear equation */
+        put_pixel(surf, (int)x, (int)y, color);
+    }
+    
 }
 
 void draw_polygons(SDL_Surface *surf, polygon *polys);
@@ -30,6 +45,7 @@ int main(int argc, char **argv) {
     
     int quit = 0;
     SDL_Event event;
+    float x1=0, y1=0;
     while(!quit) {
         
         /* process events */
@@ -39,8 +55,24 @@ int main(int argc, char **argv) {
                     quit = 1;
                     break;
                 case SDL_KEYDOWN:
-                    if(event.key.keysym.sym == SDLK_ESCAPE) {
-                        quit = 1;
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            quit = 1;
+                            break;
+                        case SDLK_w:
+                            y1 = y1 - 5.5;
+                            break;
+                        case SDLK_a:
+                            x1 = x1 - 5.5;
+                            break;
+                        case SDLK_s:
+                            y1 = y1 + 5.5;
+                            break;
+                        case SDLK_d:
+                            x1 = x1 + 5.5;
+                            break;  
+                        default:
+                            break;
                     }
                     break;
                 default:
@@ -50,13 +82,11 @@ int main(int argc, char **argv) {
         
         
         SDL_FillRect(screen, NULL, 033); /* Fill whole screen with light blue. */
-        
-        int i;
-        for(i=0; i<100; i++)
-        putpx(screen, i, 100, (Uint8)0);
+        draw_line(screen, x1, 300, y1, 100, (Uint8)0); /* Black line. */
 
         SDL_UpdateRect(screen, 0, 0, 0, 0); /* Update whole screen. */
         SDL_Delay(1000/MAX_FPS); /* Limit FPS. */
+        fflush(stdout);
     }
     
     return 0;
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 SRC=$(wildcard *.c)
 start: $(SRC)
-	gcc -Wall $(SRC) -o $@ `sdl-config --libs --cflags`
+	gcc -g -Wall $(SRC) -o $@ `sdl-config --libs --cflags`
 static:
 	gcc -static -Wall -ansi -O2 -march=native $(SRC) -o cube `sdl-config --static-libs --cflags`
